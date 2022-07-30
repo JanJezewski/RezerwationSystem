4e26670bb92b5aa8ab04adf6f98766b7a5be5c42
JanJezewski <janjezewski01@gmail.com>
2022-07-30 13:32:58 +0200

    initproject

---
+++ src/main/java/pl/rezerwationSystem/RoomRezerwationSystem/RoomRezerwationSystemApplication.java
@@ -0,0 +1,13 @@
+package pl.rezerwationSystem.RoomRezerwationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomRezerwationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomRezerwationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/rezerwationSystem/RoomRezerwationSystem/RoomRezerwationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.rezerwationSystem.RoomRezerwationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomRezerwationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
